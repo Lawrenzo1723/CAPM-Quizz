8ae00e473353189768adff5020196b39192a2133
Lawrenzo1723 <lpaul422@gmail.com>
2024-10-29 14:19:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ const domainStructure = {
 // Load questions from Google Sheets
 async function loadQuestions() {
     try {
-        const response = await fetch('https://script.google.com/macros/s/AKfycbzaiUvJLr_nyfvALBnum-xMpyd8iYUkhhVXzHhoqQ/exec');
+        const response = await fetch('https://script.google.com/macros/s/AKfycbwaHaBRWiOF7kRwffa6rv4-7G9T_JDc9BwKhdbmd6Ig76FPHrDJNjim3yGeRtEH/exec');
         const data = await response.text();
         const rows = data.split('\n');
 
